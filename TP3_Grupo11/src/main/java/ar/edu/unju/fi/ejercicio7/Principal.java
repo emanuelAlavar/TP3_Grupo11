aefb8dc0f9a394457079eba830d849a1295030ae
NoJsNoLife <87404587+NoJsNoLife@users.noreply.github.com>
2022-05-18 20:01:30 -0300

    Update Principal.java
    Agrego los cambios necesarios para su compilación:
    
    line 10 - @SpringBootApplication

--- TP3_Grupo11/src/main/java/ar/edu/unju/fi/ejercicio7/Principal.java
+++ TP3_Grupo11/src/main/java/ar/edu/unju/fi/ejercicio7/Principal.java
@@ -5,7 +5,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Principal {
 
 	public static void main(String[] args) {
